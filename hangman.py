6895309843c446a95704f90cb7577b5abf77c905
LAKRADE Salma <118934300+LakradeSalma@users.noreply.github.com>
2023-01-28 22:44:37 +0100

    Code
    The code.

--- hangman.py
+++ hangman.py
@@ -17,19 +17,24 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()  # letters guessed
 
-    while len(word_letters) > 0:
+    lives = 6
+    while len(word_letters) > 0 and lives > 0:
         # letters guessed
-        print("You gueesed these letters : ", " ".join(used_letters))
+        print("You have",lives, "lives. You gueesed these letters : ", " ".join(used_letters))
 
         # the current word
         word_list = [letter if letter in used_letters else "-" for letter in word]
         print("The word : ", " ".join(word_list))
 
-        player_guessing = input("Guess a letter : ").upper()
+        player_guessing = input(" \nGuess a letter : ").upper()
         if player_guessing in alphabet - used_letters:
             used_letters.add(player_guessing)
             if player_guessing in word_letters:
                 word_letters.remove(player_guessing)
+
+            else:
+                lives = lives - 1
+                print("Letter not in the word. ")
 
         elif player_guessing in used_letters:
             print("You already guessed this character. Choose another one. ")
@@ -37,6 +42,9 @@ def hangman():
         else:
             print("Invalid character. Try again. ")
 
-    print("Congrats, you gueesed the word", word, "correcty! ")
+    if lives == 0:
+        print("You died. The word was ",word, ". Try again! ")
+    else:
+        print("Congrats, you gueesed the word", word, "correcty! ")
 
 hangman()
