795acb1ab5c7d67d36ed715482e03f49706e6574
LAKRADE Salma <118934300+LakradeSalma@users.noreply.github.com>
2023-01-28 22:35:49 +0100

    Code
    The code.

---
+++ hangman.py
@@ -0,0 +1,42 @@
+import string
+import random
+
+words = ['abruptly', 'absurd', 'abyss', 'affix', 'askew', 'avenue', 'awkward', 'axiom', 'azure', 'bagpipes',
+         'bandwagon', 'banjo', 'bayou', 'beekeeper', 'bikini', 'blitz', 'blizzard', 'boggle', 'bookworm', 'boxcar',
+         'boxful', 'buckaroo', 'buffalo', 'buffoon', 'buxom', 'buzzard', 'buzzing', 'buzzwords', 'caliph', 'cobweb',
+         'cockiness', 'croquet', 'crypt', 'curacao', 'cycle', 'daiquiri', 'dirndl', 'disavow', 'dizzying', 'duplex',
+         'dwarves', 'embezzle', 'equip', 'espionage', 'euouae', 'exodus', 'faking', 'fishhook', 'fixable', 'fjord',
+         'flapjack', 'flopping', 'fluffiness', 'flyby', 'foxglove', 'frazzled', 'frizzled', 'fuchsia', 'funny', 'gabby',
+         'galaxy', 'galvanize', 'gazebo', 'giaour', 'gizmo', 'glowworm', 'glyph', 'gnarly', 'gnostic', 'gossip',
+         'rogginess']
+
+
+def hangman():
+    word = random.choice(words).upper()
+    word_letters = set(word)  # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()  # letters guessed
+
+    while len(word_letters) > 0:
+        # letters guessed
+        print("You gueesed these letters : ", " ".join(used_letters))
+
+        # the current word
+        word_list = [letter if letter in used_letters else "-" for letter in word]
+        print("The word : ", " ".join(word_list))
+
+        player_guessing = input("Guess a letter : ").upper()
+        if player_guessing in alphabet - used_letters:
+            used_letters.add(player_guessing)
+            if player_guessing in word_letters:
+                word_letters.remove(player_guessing)
+
+        elif player_guessing in used_letters:
+            print("You already guessed this character. Choose another one. ")
+
+        else:
+            print("Invalid character. Try again. ")
+
+    print("Congrats, you gueesed the word", word, "correcty! ")
+
+hangman()
